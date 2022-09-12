faa99c60584fe33ee6d238987851e35e6cd55aea
pojojojo21 <65415823+pojojojo21@users.noreply.github.com>
2022-09-12 02:12:00 -0400

    Through Question 4

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import {vec3} from 'gl-matrix';
+import {vec3, vec4} from 'gl-matrix';
 const Stats = require('stats-js');
 import * as DAT from 'dat.gui';
 import Icosphere from './geometry/Icosphere';
 import Square from './geometry/Square';
+import Cube from './geometry/Cube';
 import OpenGLRenderer from './rendering/gl/OpenGLRenderer';
 import Camera from './Camera';
 import {setGL} from './globals';
@@ -13,17 +14,26 @@ import ShaderProgram, {Shader} from './rendering/gl/ShaderProgram';
 const controls = {
   tesselations: 5,
   'Load Scene': loadScene, // A function pointer, essentially
+  red: 255,
+  green: 0,
+  blue: 0,
 };
 
 let icosphere: Icosphere;
 let square: Square;
+let cube: Cube;
 let prevTesselations: number = 5;
+let prevRed: number = 255;
+let prevGreen: number = 0;
+let prevBlue: number = 0;
 
 function loadScene() {
   icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, controls.tesselations);
   icosphere.create();
   square = new Square(vec3.fromValues(0, 0, 0));
   square.create();
+  cube = new Cube(vec3.fromValues(0, 0, 0));
+  cube.create();
 }
 
 function main() {
@@ -39,6 +49,9 @@ function main() {
   const gui = new DAT.GUI();
   gui.add(controls, 'tesselations', 0, 8).step(1);
   gui.add(controls, 'Load Scene');
+  gui.add(controls, 'red', 0, 255).step(1);
+  gui.add(controls, 'green', 0, 255).step(1);
+  gui.add(controls, 'blue', 0, 255).step(1);
 
   // get canvas and webgl context
   const canvas = <HTMLCanvasElement> document.getElementById('canvas');
@@ -64,6 +77,11 @@ function main() {
     new Shader(gl.FRAGMENT_SHADER, require('./shaders/lambert-frag.glsl')),
   ]);
 
+  // const custom1 = new ShaderProgram([
+  //   new Shader(gl.VERTEX_SHADER, require('./shaders/lambert-vert.glsl')),
+  //   new Shader(gl.FRAGMENT_SHADER, require('./shaders/custom-frag.glsl')),
+  // ]);
+
   // This function will be called every frame
   function tick() {
     camera.update();
@@ -75,10 +93,27 @@ function main() {
       prevTesselations = controls.tesselations;
       icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, prevTesselations);
       icosphere.create();
+    }    
+    if(controls.red != prevRed)
+    {
+      prevRed = controls.red
     }
+    if(controls.green != prevGreen)
+    {
+      prevGreen = controls.green
+    }
+    if(controls.blue != prevBlue)
+    {
+      prevBlue = controls.blue
+    }
+
+    lambert.setGeometryColor(vec4.fromValues(controls.red/255., controls.green/255., controls.blue/255., 1));
+    // custom1.setGeometryColor(vec4.fromValues(controls.red/255., controls.green/255., controls.blue/255., 1));
+
     renderer.render(camera, lambert, [
-      icosphere,
+      // icosphere,
       // square,
+      cube,
     ]);
     stats.end();
 
---
+++ src/shaders/custom-frag.glsl
@@ -0,0 +1,76 @@
+#version 300 es
+
+// This is a fragment shader. If you've opened this file first, please
+// open and read lambert.vert.glsl before reading on.
+// Unlike the vertex shader, the fragment shader actually does compute
+// the shading of geometry. For every pixel in your program's output
+// screen, the fragment shader is run for every bit of geometry that
+// particular pixel overlaps. By implicitly interpolating the position
+// data passed into the fragment shader by the vertex shader, the fragment shader
+// can compute what color to apply to its pixel based on things like vertex
+// position, light position, and vertex color.
+precision highp float;
+
+uniform vec4 u_Color; // The color with which to render this instance of geometry.
+
+// These are the interpolated values out of the rasterizer, so you can't know
+// their specific values without knowing the vertices that contributed to them
+in vec4 fs_Nor;
+in vec4 fs_LightVec;
+in vec4 fs_Col;
+
+out vec4 out_Col; // This is the final output color that you will see on your
+                  // screen for the pixel that is currently being processed.
+
+float perlinNoise3D(vec3 p) {
+    float surfletSum = 0.f;
+    // Iterate over the four integer corners surrounding uv
+    for(int dx = 0; dx <= 1; ++dx) {
+        for(int dy = 0; dy <= 1; ++dy) {
+            for(int dz = 0; dz <= 1; ++dz) {
+                surfletSum += surflet3d(p, floor(p) + vec3(dx, dy, dz));
+            }
+        }
+    }
+    return surfletSum;
+}
+
+float surflet3d(vec3 p, vec3 gridPoint) {
+    // Compute the distance between p and the grid point along each axis, and warp it with a
+    // quintic function so we can smooth our cells
+    vec3 t2 = abs(p - gridPoint);
+    vec3 t = vec3(1.f) - 6.f * vec3(pow(t2[0], 5.f),pow(t2[1], 5.f),pow(t2[2], 5.f)) + 15.f *
+            vec3(pow(t2[0], 4.f),pow(t2[1], 4.f),pow(t2[2], 4.f)) - 10.f *
+            vec3(pow(t2[0], 3.f),pow(t2[1], 3.f),pow(t2[2], 3.f));
+    // Get the random vector for the grid point (assume we wrote a function random2
+    // that returns a vec2 in the range [0, 1])
+    vec3 gradient = normalize(random3(gridPoint) * 2.f - vec3(1., 1., 1.));
+    // Get the vector from the grid point to P
+    vec3 diff = p - gridPoint;
+    // Get the value of our height field by dotting grid->P with our gradient
+    float height = dot(diff, gradient);
+    // Scale our height field (i.e. reduce it) by our polynomial falloff function
+    return height * t.x * t.y * t.z;
+}
+
+void main()
+{
+    // Material base color (before shading)
+        vec4 diffuseColor = u_Color;
+
+        diffuseColor = diffuseColor + vec4(perlinNoise3D(diffuseColor.xyz), 0)
+
+        // Calculate the diffuse term for Lambert shading
+        float diffuseTerm = dot(normalize(fs_Nor), normalize(fs_LightVec));
+        // Avoid negative lighting values
+        // diffuseTerm = clamp(diffuseTerm, 0, 1);
+
+        float ambientTerm = 0.2;
+
+        float lightIntensity = diffuseTerm + ambientTerm;   //Add a small float value to the color multiplier
+                                                            //to simulate ambient lighting. This ensures that faces that are not
+                                                            //lit by our point light are not completely black.
+
+        // Compute final shaded color
+        out_Col = vec4(diffuseColor.rgb * lightIntensity, diffuseColor.a);
+}
